ddad907e1b14b8bf1a231f71e5229907bd50bd21
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-03-17 15:36:47 +0200

    Implement user login & redirect to landing page

--- backend/login.php
+++ backend/login.php
@@ -1,13 +1,13 @@
 <?php
 include("connection.php");
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+header("Access-Control-Allow-Headers: Content-Type");
+
 $email = $_POST['email'] ?? '';
 $password = $_POST['password'] ?? '';
-
-$query = $mysqli->prepare('SELECT id, username,  email,  password, role FROM users WHERE email=?');
-$query->bind_param('s', $email);
-$query->execute();
-$query->store_result();
 
 ///* this'll be implemented later
 // $query->bind_result($id, $username, $email, $hashed_password, $role);
@@ -32,13 +32,15 @@ $query->store_result();
 
 // echo json_encode($response);
 
-
+$query = $mysqli->prepare('select id, username, email,  password, role FROM users WHERE email=?');
+$query->bind_param('s', $email);
+$query->execute();
+$query->store_result();
 $query->bind_result($id, $username, $email, $password, $role);
 $query->fetch();
 $num_rows = $query->num_rows();
 
-
-if ($num_rows === 1) {
+if ($num_rows == 1) {
     $response['status'] = "success";
     $response['id'] = $id;
     $response['username'] = $username;
@@ -47,5 +49,4 @@ if ($num_rows === 1) {
     $response['status'] = 'fail';
     $responsen['message'] = 'user not found';
 }
-
 echo json_encode($response);
--- frontend/scripts/app/login-register.js
+++ frontend/scripts/app/login-register.js
@@ -1,10 +1,61 @@
 const loginForm = document.getElementById('login-form');
-const userName = document.getElementById('user-email');
+const userEmail = document.getElementById('user-email');
 const userPassword = document.getElementById('user-password');
+
+// const userLogin = () => {
+//   const url = 'http://localhost/Flight-System-Website/backend/login.php';
+//   const formatData = new FormData();
+
+//   formatData.append('email', userEmail.value);
+//   formatData.append('password', userPassword.value);
+
+//   const options = {
+//     method: 'POST',
+//     body: formatData,
+//   };
+
+//   fetch(url, options)
+//     .then((res) => res.json())
+//     .then((data) => {
+//       if (data['status'] === 'success') {
+//         localStorage.setItem('id', data['id']);
+//         window.location.href = 'http://127.0.0.1:5500/frontend/pages/landingPage.html';
+//       }
+//     });
+// };
+
+const userLogin = async () => {
+  const url = 'http://localhost/Flight-System-Website/backend/login.php';
+  const formatData = new FormData();
+
+  formatData.append('email', userEmail.value);
+  formatData.append('password', userPassword.value);
+
+  const options = {
+    method: 'POST',
+    body: formatData,
+  };
+
+  try {
+    const res = await fetch(url, options);
+    const data = await res.json();
+
+    if (data['status'] === 'success') {
+      localStorage.setItem('id', data['id']);
+      window.location.href = 'http://127.0.0.1:5500/frontend/pages/landingPage.html';
+    } else {
+      console.log('incoorect credentials');
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 loginForm.addEventListener('submit', (e) => {
   e.preventDefault();
 
-  console.log(userName.value);
+  console.log(userEmail.value);
   console.log(userPassword.value);
+
+  userLogin();
 });
