c0e4b4093e5e0155c07bccab6630e0b2a3f93fe0
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-03-20 06:21:03 +0200

    Implement Rendering Flights infos

---
+++ client/scripts/app/admin/crud.js
@@ -0,0 +1,43 @@
+const flightsInfosContainer = document.getElementById('flights-infos-container');
+const path = 'http://localhost/Flight-System-Website/server/admin/flights';
+
+const getFlightsInfos = async () => {
+  const url = `${path}/read.php`;
+  const response = await fetch(url);
+  const infos = await response.json();
+  console.log(infos);
+
+  renderFlightsInfos(infos);
+};
+
+const renderFlightsInfos = ({ status, flights }) => {
+  flightsInfosContainer.innerHTML = '';
+
+  flights.forEach((flight) => {
+    flightsInfosContainer.innerHTML += flightComponent(flight);
+  });
+};
+
+const flightComponent = ({
+  flight_id,
+  destination,
+  departure_date,
+  arrival_date,
+  departure_airport,
+  airline_id,
+  arrival_airport,
+  price,
+}) => `<tr class="flights-info">
+<td class="flight-number">${flight_id}</td>
+<td>${destination}</td>
+<td>${airline_id}</td>
+<td>${departure_date}</td>
+<td>${arrival_date}</td>
+<td>${departure_airport}</td>
+<td>${arrival_airport}</td>
+<td>${price}</td>
+<td id="edit-flight">Edit</td>
+<td id="delete-flight">Delete</td>
+</tr>`;
+
+getFlightsInfos();
--- server/connection.php
+++ server/connection.php
@@ -1,5 +1,14 @@
 <?php
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+header("Access-Control-Allow-Headers: Content-Type");
+
+
+
 $mysqli = new mysqli('localhost', 'root', "", "flightsystemwebsite");
+
 
 if ($mysqli->connect_error) {
     die("Connection Error (" . $mysqli->connect_errno . ')' . $mysqli->connect_error);
