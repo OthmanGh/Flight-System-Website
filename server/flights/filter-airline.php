5a7570ffe5b278363f182e03e542717fb7f0bb74
yasmine776766 <yis04@mail.aub.edu>
2024-03-20 06:31:57 +0200

    Imlement filter of flights by airline

---
+++ server/flights/filter-airline.php
@@ -0,0 +1,43 @@
+<?php
+
+include("../connection.php");
+
+$airline = $_POST['airline'];
+$destination = $_POST['destination'];
+$departure_date = $_POST['departure_date'];
+$arrival_date = $_POST['arrival_date'];
+$price = $_POST['price'];
+
+
+$query = $mysqli->prepare("SELECT destination, departure_date, arrival_date, price FROM flights WHERE airline=?");
+$query->bind_param('s', $airline);
+$query->execute();
+$query->store_result();
+$query->bind_result($destination, $departure_date, $arrival_date, $price);
+$num_rows = $query->num_rows();
+if ($num_rows == 0) {
+        $response['status'] = 'fail';
+        $response['message'] = 'No flights match your filtering at the moment';
+    } else {
+        $flights = [];
+        while ($query->fetch()) {
+            $flight = [
+                'destination' => $destination,
+                'departure_date' => $departure_date,
+                'arrival_date' => $arrival_date,
+                'price' => $price
+            ];
+            $flights[] = $flight;
+        }
+        $response['status'] = 'success';
+        $response['flights'] = $flights;
+    }
+
+echo json_encode($response);
+
+
+
+
+
+
+    
