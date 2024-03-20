2c1694adfd5f4b0470f9671f7d2cdbc727d590ac
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-03-20 08:36:23 +0200

    Modify delete api & implement deleting functionality to the UI

--- client/scripts/app/admin/crud.js
+++ client/scripts/app/admin/crud.js
@@ -1,23 +1,6 @@
 const flightsInfosContainer = document.getElementById('flights-infos-container');
 const path = 'http://localhost/Flight-System-Website/server/admin/flights';
 
-const getFlightsInfos = async () => {
-  const url = `${path}/read.php`;
-  const response = await fetch(url);
-  const infos = await response.json();
-  console.log(infos);
-
-  renderFlightsInfos(infos);
-};
-
-const renderFlightsInfos = ({ status, flights }) => {
-  flightsInfosContainer.innerHTML = '';
-
-  flights.forEach((flight) => {
-    flightsInfosContainer.innerHTML += flightComponent(flight);
-  });
-};
-
 const flightComponent = ({
   flight_id,
   destination,
@@ -28,20 +11,60 @@ const flightComponent = ({
   arrival_airport,
   price,
 }) => `<tr class="flights-info">
-<td class="flight-number">${flight_id}</td>
-<td>${destination}</td>
-<td>${airline_id}</td>
-<td>${departure_date}</td>
-<td>${arrival_date}</td>
-<td>${departure_airport}</td>
-<td>${arrival_airport}</td>
-<td>${price}</td>
-<td id="delete-flight">
-<img alt="edit logo" src="http://localhost/Flight-System-Website/client/assets/edit.svg" />
-</td>
-<td class="edit-logo" id="edit-flight">
-<img class="trash-logo" alt="trash logo" src="http://localhost/Flight-System-Website/client/assets/trash.svg" />
-</td>
-</tr>`;
+  <td class="flight-number">${flight_id}</td>
+  <td>${destination}</td>
+  <td>${airline_id}</td>
+  <td>${departure_date}</td>
+  <td>${arrival_date}</td>
+  <td>${departure_airport}</td>
+  <td>${arrival_airport}</td>
+  <td>${price}</td>
 
-getFlightsInfos();
+  <td class="edit-flight">
+  <img class="edit-btn" id="edit-btn" alt="edit logo" src="http://localhost/Flight-System-Website/client/assets/edit.svg" />
+  </td>
+  <td  class="delete-flight">
+  <img class="delete-btn" id="delete-btn"  alt="trash logo" src="http://localhost/Flight-System-Website/client/assets/trash.svg" />
+  </td>
+  </tr>`;
+
+const renderFlightsInfos = ({ status, flights }) => {
+  flightsInfosContainer.innerHTML = '';
+
+  flights.forEach((flight) => {
+    flightsInfosContainer.innerHTML += flightComponent(flight);
+  });
+};
+
+const deleteFlight = async (flightId) => {
+  try {
+    const url = `${path}/delete.php?flight_id=${flightId}`;
+    const response = await fetch(url);
+    const infos = await response.json();
+    renderFlightsInfos(infos);
+  } catch (error) {
+    console.error('Error deleting flight:', error);
+  }
+};
+
+const readFlights = async () => {
+  const url = `${path}/read.php`;
+  const response = await fetch(url);
+  const infos = await response.json();
+
+  console.log(infos);
+  renderFlightsInfos(infos);
+};
+
+flightsInfosContainer.addEventListener('click', async (e) => {
+  if (e.target.classList.contains('delete-btn')) {
+    const flightRow = e.target.closest('.flights-info'); // Selecting parent
+
+    if (flightRow) {
+      const flightId = flightRow.querySelector('.flight-number').textContent; // Searching down
+      await deleteFlight(flightId);
+    }
+  }
+});
+
+readFlights();
--- server/admin/flights/delete.php
+++ server/admin/flights/delete.php
@@ -3,17 +3,25 @@ include('../../connection.php');
 
 $flight_id = $_GET['flight_id'];
 
-$query = $mysqli->prepare('DELETE FROM flights WHERE flight_id =?');
-$query->bind_param('i', $flight_id);
+$query_delete = $mysqli->prepare('DELETE FROM flights WHERE flight_id = ?');
+$query_delete->bind_param('i', $flight_id);
 
+if ($query_delete->execute()) {
+    $query_select = $mysqli->prepare('SELECT * FROM flights');
+    $query_select->execute();
+    $result = $query_select->get_result();
 
-if ($query->execute()) {
-    $response['success'] = 'success';
-    $response['message'] = "Flight is deleted successfully";
+    $flights = array();
+    while ($row = $result->fetch_assoc()) {
+        $flights[] = $row;
+    }
+
+    $response['status'] = 'success';
+    $response['message'] = 'Flight is deleted successfully';
+    $response['flights'] = $flights;
 } else {
-    $response['status'] =  'fail';
+    $response['status'] = 'fail';
     $response['message'] = 'Failed to delete Flight';
 }
-
 
 echo json_encode($response);
