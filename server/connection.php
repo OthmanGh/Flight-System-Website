bedbc0923f105632f26d713be91d7fe4cacafedb
yasmine776766 <yis04@mail.aub.edu>
2024-03-21 05:24:38 +0200

    Implement link between landing and booking page

--- server/connection.php
+++ server/connection.php
@@ -1,4 +1,9 @@
 <?php
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+header("Access-Control-Allow-Headers: Content-Type");
 $mysqli = new mysqli('localhost', 'root', "", "flightssystemwebsite");
 
 if ($mysqli->connect_error) {
